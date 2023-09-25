b1a72fa4bbe14574bc47ec84e25ef9ae047bbb3c
Marcos-Contreras <marcosalonso2230@gmail.com>
2023-09-25 17:27:53 -0600

    feat: :sparkles: use FormGroup and handle validation errors

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -8,19 +8,7 @@ import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms'
 })
 export class BasicFormComponent implements OnInit {
 
-  form = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    date: new FormControl(''),
-    color: new FormControl('#000000'),
-    age: new FormControl('12'),
-    category: new FormControl(''),
-    tags: new FormControl(''),
-    agree: new FormControl(false),
-    gender: new FormControl(''),
-    zone: new FormControl('')
-  });
+  form: FormGroup;
 
   // FormControl HAS THREE PARAMETERS: INITAL VALUE, SYNC AND ASYNC FUNCTIONS
   // name = new FormControl('', [Validators.required, Validators.maxLength(10)]);
@@ -39,10 +27,28 @@ export class BasicFormComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder
-  ) { }
+  ) {
+    this.buildForm();
+  }
 
   buildForm() {
-    
+    // EVERY FIELD HAS THREE PARAMETERS: INITAL VALUE, SYNC AND ASYNC FUNCTIONS
+    this.form = this.formBuilder.group({
+      name: [
+        '',
+        [Validators.required, Validators.maxLength(10), Validators.pattern(/^[a-zA-Z]+$/)]
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      date: [''],
+      color: ['#000000'],
+      age: [12, [Validators.required, Validators.min(18), Validators.max(100)]],
+      category: [''],
+      tags: [''],
+      agree: [false, [Validators.requiredTrue]],
+      gender: [''],
+      zone: ['']
+    });
   }
 
   ngOnInit(): void {
@@ -59,16 +65,37 @@ export class BasicFormComponent implements OnInit {
   }
 
   save() {
-    console.log(this.form.value);
-
+    if(this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      this.form.markAllAsTouched();
+    }
   }
 
   // GETTERS
   get isNameFieldValid() {
-    return this.form.value.name.touched && this.form.value.name.valid;
+    return this.form.get('name').touched && this.form.get('name').valid;
   }
   get isNameFieldInvalid() {
-    return this.form.value.name.touched && this.form.value.name.invalid;
+    return this.form.get('name').touched && this.form.get('name').invalid;
+  }
+  get isPhoneFieldValid() {
+    return this.form.get('phone').touched && this.form.get('phone').valid;
+  }
+  get isPhoneFieldInvalid() {
+    return this.form.get('phone').touched && this.form.get('phone').invalid;
+  }
+  get isEmailFieldValid() {
+    return this.form.get('email').touched && this.form.get('email').valid;
+  }
+  get isEmailFieldInvalid() {
+    return this.form.get('email').touched && this.form.get('email').invalid;
+  }
+  get isAgeFieldValid() {
+    return this.form.get('age').touched && this.form.get('age').valid;
+  }
+  get isAgeFieldInvalid() {
+    return this.form.get('age').touched && this.form.get('age').invalid;
   }
   get name() {
     return this.form.get('name');
